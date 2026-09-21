e6c7ffa3d068efa6359720ecb08290538edf7f2e
45kikurage-rgb <45kikurage@gmail.com>
2026-09-22 00:27:48 +0900

    Entry Manager: refresh PWA cache v4

--- entry-manager/sw.js
+++ entry-manager/sw.js
@@ -1,9 +1,9 @@
-const CACHE = 'entry-manager-20260922-1';
+const CACHE = 'entry-manager-20260922-2';
 const ASSETS = [
   './',
   './index.html',
-  './styles.css?v=20260922-1',
-  './app.js?v=20260922-1',
+  './styles.css?v=20260922-2',
+  './app.js?v=20260922-2',
   './manifest.webmanifest',
   './icon.svg'
 ];
