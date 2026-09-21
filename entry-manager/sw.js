369d9592c7d841f7d1caf593170d8232f3b562c3
45kikurage-rgb <45kikurage@gmail.com>
2026-09-21 23:52:48 +0900

    Sync entry-manager/sw.js from main for published Entry Manager

--- entry-manager/sw.js
+++ entry-manager/sw.js
@@ -1,9 +1,9 @@
-const CACHE = 'entry-manager-20260921-1';
+const CACHE = 'entry-manager-20260921-3';
 const ASSETS = [
   './',
   './index.html',
-  './styles.css?v=20260921-1',
-  './app.js?v=20260921-1',
+  './styles.css?v=20260921-3',
+  './app.js?v=20260921-3',
   './manifest.webmanifest',
   './icon.svg'
 ];
