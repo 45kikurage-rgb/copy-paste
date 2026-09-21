96e39848c9614b793502e7909f7e49ef2dde7ce8
45kikurage-rgb <45kikurage@gmail.com>
2026-09-21 11:36:45 +0900

    feat(entry-manager): add offline service worker

---
+++ entry-manager/sw.js
@@ -0,0 +1,34 @@
+const CACHE = 'entry-manager-20260921-1';
+const ASSETS = [
+  './',
+  './index.html',
+  './styles.css?v=20260921-1',
+  './app.js?v=20260921-1',
+  './manifest.webmanifest',
+  './icon.svg'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(key => key.startsWith('entry-manager-') && key !== CACHE).map(key => caches.delete(key))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  if(event.request.method !== 'GET') return;
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE).then(cache => cache.put(event.request, copy));
+        return response;
+      })
+      .catch(() => caches.match(event.request).then(hit => hit || caches.match('./index.html')))
+  );
+});
